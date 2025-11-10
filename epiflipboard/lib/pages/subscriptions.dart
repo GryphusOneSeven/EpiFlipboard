788a5ab833df520c147f39e13c9b69f1b0706ce3
GryphusOneSeven <gael.edmond@epitech.eu>
2025-11-10 14:10:26 +0400

    [ADD] - Added pages and navigation

---
+++ epiflipboard/lib/pages/subscriptions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SubscriptionsPage extends StatelessWidget {
+  const SubscriptionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Subscriptions Page',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
