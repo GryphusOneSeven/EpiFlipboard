ac3b41fad709698f03920a41405aac1c3b081522
Deathm12 <jerome.paton@epitech.eu>
2025-12-04 15:10:07 +0400

    [FIX] - Untrack generated files - update requirements.txt

--- epiflipboard/backend/app/main.py
+++ epiflipboard/backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.db import database  # assume package/app layout
+# from app.db import database  # assume package/app layout
 
 app = FastAPI()
 
