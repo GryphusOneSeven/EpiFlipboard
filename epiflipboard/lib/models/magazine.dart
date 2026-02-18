73de1bbe428b0e1cfc2734e726480a944e0c724e
GryphusOneSeven <gael.edmond@epitech.eu>
2026-02-18 11:54:47 +0400

    [ADD] - Magazine db model

---
+++ epiflipboard/lib/models/magazine.dart
@@ -0,0 +1,20 @@
+class Magazine {
+  final String name;
+  final String description;
+  final String private;
+
+  Magazine({
+    required this.name,
+    required this.description,
+    required this.private,
+  });
+
+  factory Magazine.fromJson(Map<String, dynamic> json) {
+    return Magazine(
+      name: json["name"] ?? "",
+      description: json["description"] ?? "",
+      private: json["private"] ?? false,
+    );
+  }
+
+}
