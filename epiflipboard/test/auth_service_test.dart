c367b5af6b6ebc50a48dad778ab99fe7c18e8afd
Deathm12 <jerome.paton@epitech.eu>
2026-02-18 17:36:20 +0400

    [UPD] - test for auth_service.dart and auth_storage.dart

--- epiflipboard/test/auth_service_test.dart
+++ epiflipboard/test/auth_service_test.dart
@@ -7,19 +7,15 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:epiflipboard/services/auth_service.dart';
 import 'package:epiflipboard/services/google_sign_in_wrapper.dart';
+import 'package:epiflipboard/services/auth_storage.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 // ---------------- MOCKS ----------------
-
 class MockGoogleSignIn extends Mock implements IGoogleSignIn {}
-
-class MockGoogleSignInAccount extends Mock
-    implements GoogleSignInAccount {}
-
-class MockGoogleSignInAuthentication extends Mock
-    implements GoogleSignInAuthentication {}
-
+class MockGoogleSignInAccount extends Mock implements GoogleSignInAccount {}
+class MockGoogleSignInAuthentication extends Mock implements GoogleSignInAuthentication {}
 class MockHttpClient extends Mock implements http.Client {}
-
+class MockFlutterSecureStorage extends Mock implements FlutterSecureStorage {}
 class FakeUri extends Fake implements Uri {}
 
 void main() {
@@ -31,10 +27,15 @@ void main() {
   late AuthService authService;
   late MockGoogleSignIn mockGoogleSignIn;
   late MockHttpClient mockClient;
+  late MockFlutterSecureStorage mockStorage;
 
   setUp(() {
     mockGoogleSignIn = MockGoogleSignIn();
     mockClient = MockHttpClient();
+    mockStorage = MockFlutterSecureStorage();
+
+    // Injection du mock storage
+    AuthStorage.testStorage = mockStorage;
 
     authService = AuthService(
       googleSignIn: mockGoogleSignIn,
@@ -42,14 +43,10 @@ void main() {
     );
   });
 
-
   group('AuthService.signInWithGoogle', () {
     test('retourne false si l\'utilisateur annule la connexion', () async {
-      when(() => mockGoogleSignIn.signOut())
-          .thenAnswer((_) => Future.value());
-
-      when(() => mockGoogleSignIn.signIn())
-          .thenAnswer((_) async => null);
+      when(() => mockGoogleSignIn.signOut()).thenAnswer((_) async {});
+      when(() => mockGoogleSignIn.signIn()).thenAnswer((_) async => null);
 
       final result = await authService.signInWithGoogle();
 
@@ -60,57 +57,43 @@ void main() {
       final mockAccount = MockGoogleSignInAccount();
       final mockAuth = MockGoogleSignInAuthentication();
 
-      when(() => mockGoogleSignIn.signOut())
-          .thenAnswer((_) => Future.value());
+      // Mocks Google SignIn
+      when(() => mockGoogleSignIn.signOut()).thenAnswer((_) async {});
+      when(() => mockGoogleSignIn.signIn()).thenAnswer((_) async => mockAccount);
+      when(() => mockAccount.authentication).thenAnswer((_) async => mockAuth);
+      when(() => mockAuth.idToken).thenReturn('fake_id_token');
 
-      when(() => mockGoogleSignIn.signIn())
-          .thenAnswer((_) async => mockAccount);
+      // Mock storage
+      when(() => mockStorage.write(key: any(named: 'key'), value: any(named: 'value')))
+          .thenAnswer((_) async {});
 
-      when(() => mockAccount.authentication)
-          .thenAnswer((_) async => mockAuth);
-
-      when(() => mockAuth.idToken)
-          .thenReturn('fake_id_token');
-
+      // Mock HTTP
       when(() => mockClient.post(
-            any(),
+            any<Uri>(),
             headers: any(named: 'headers'),
             body: any(named: 'body'),
-          )).thenAnswer(
-        (_) async => http.Response(
-          jsonEncode({'token': 'jwt_token'}),
-          200,
-        ),
-      );
+          )).thenAnswer((_) async => http.Response(jsonEncode({'token': 'jwt_token'}), 200));
 
       final result = await authService.signInWithGoogle();
 
       expect(result, true);
+      verify(() => mockStorage.write(key: 'jwt', value: 'jwt_token')).called(1);
     });
 
     test('retourne false si post HTTP échoue', () async {
       final mockAccount = MockGoogleSignInAccount();
       final mockAuth = MockGoogleSignInAuthentication();
 
-      when(() => mockGoogleSignIn.signOut())
-          .thenAnswer((_) => Future.value());
-
-      when(() => mockGoogleSignIn.signIn())
-          .thenAnswer((_) async => mockAccount);
-
-      when(() => mockAccount.authentication)
-          .thenAnswer((_) async => mockAuth);
-
-      when(() => mockAuth.idToken)
-          .thenReturn('fake_id_token');
+      when(() => mockGoogleSignIn.signOut()).thenAnswer((_) async {});
+      when(() => mockGoogleSignIn.signIn()).thenAnswer((_) async => mockAccount);
+      when(() => mockAccount.authentication).thenAnswer((_) async => mockAuth);
+      when(() => mockAuth.idToken).thenReturn('fake_id_token');
 
       when(() => mockClient.post(
-            any(),
+            any<Uri>(),
             headers: any(named: 'headers'),
             body: any(named: 'body'),
-          )).thenAnswer(
-        (_) async => http.Response('Erreur', 500),
-      );
+          )).thenAnswer((_) async => http.Response('Erreur', 500));
 
       final result = await authService.signInWithGoogle();
 
@@ -120,8 +103,7 @@ void main() {
 
   group('AuthService.signOut', () {
     test('appelle disconnect', () async {
-      when(() => mockGoogleSignIn.disconnect())
-          .thenAnswer((_) => Future.value());
+      when(() => mockGoogleSignIn.disconnect()).thenAnswer((_) async {});
 
       await authService.signOut();
 
