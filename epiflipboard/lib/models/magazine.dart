3aee7c55a4dc14197cc7659f5609afe29f88b55e
GryphusOneSeven <gael.edmond@epitech.eu>
2026-02-19 19:31:28 +0400

    [FIX] - add id in magazine model

--- epiflipboard/lib/models/magazine.dart
+++ epiflipboard/lib/models/magazine.dart
@@ -3,16 +3,19 @@ class Magazine {
   final String description;
   final String private;
   final int owner;
+  final int id;
 
   Magazine({
     required this.name,
     required this.description,
     required this.private,
     required this.owner,
+    required this.id,
   });
 
   factory Magazine.fromJson(Map<String, dynamic> json) {
     return Magazine(
+      id: json["id"] ?? 0,
       name: json["name"] ?? "",
       description: json["description"] ?? "",
       private: json["private"] ?? "false",
